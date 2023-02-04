3220aecda176920dfe779a32525196aeec11824a
Arnab Mondal <arxmondal@hotmail.com>
2023-02-04 21:43:27 +0530

    Update DbConnection.java

--- BankManagementSystem/src/com/bms/db/DbConnection.java
+++ BankManagementSystem/src/com/bms/db/DbConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 
 public class DbConnection {
     public static Connection connect() throws Exception {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.cj.jdbc.Driver"); 
         return DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem", "root", "Arnab@2003");
     }
 }
